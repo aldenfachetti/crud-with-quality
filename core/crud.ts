9793bc2a061436c10076f4d04a90ae2f930ec997
aldenfachetti <aldenmerlin@gmail.com>
2023-07-24 15:09:27 +0200

    CREATE - Memória vs Disco

---
+++ core/crud.ts
@@ -0,0 +1,15 @@
+import fs from "fs"; // ES6
+// const fs = require("fs"); - CommonJS
+
+const DB_FILE_PATH = "./core/db";
+
+console.log("[CRUD]");
+
+function create(content: string) {
+  // salvar o content no sistema
+  fs.writeFileSync(DB_FILE_PATH, content);
+  return content;
+}
+
+// [SIMULATION]
+console.log(create("...asdusdahhdusauhas"));
